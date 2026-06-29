c61d5f94288fb4382fd2c8b6e956ab6f7b620c2a
Sam Antony <samantony@ronsystech.com>
2026-06-30 04:18:23 +0530

    basic command for print function and their args

---
+++ index.py
@@ -0,0 +1,13 @@
+# index.py
+
+print("Python is working!", end="...", flush=True)
+print("It should concat with first line")
+
+
+# print(*values, sep=' ', end='\n', file=None, flush=False) this is the syntax for displaying output
+# *values (Collect all arguments into a tuple called *values)
+# sep=' ' (like giving space between args)
+# end='\n' (the above example is for this argument)
+# file (this destinations is called stdout)
+# flush (it helps to clear the internal memory set as boolean)
+
